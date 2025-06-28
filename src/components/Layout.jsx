58a7a3a34a1fa489ec733b40549fb50ee46d8616
nakuru172000 <150978501+nakuru172000@users.noreply.github.com>
2025-06-29 01:35:03 +0200

    background issue when browser in dark mode solved

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -6,7 +6,7 @@ import { FaArrowUp } from 'react-icons/fa';
 
 export default function Layout({ children,genres }) {
   return (
-    <div className="min-h-screen dark:bg-gray-900">
+    <div className="min-h-screen">
       <div className='sticky top-0 z-50'><Navbar /> 
       </div>
       
