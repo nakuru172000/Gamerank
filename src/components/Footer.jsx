4e160d963ffed8ccec741e3335c5733a4dae9087
nakuru172000 <150978501+nakuru172000@users.noreply.github.com>
2025-07-04 01:08:07 +0200

    bis

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,7 +8,7 @@ const Footer = () => {
          
            {/* Social Media */}
           <div>
-            <h3 className="text-xl font-bold mb-2">Follow Us</h3>
+            <h3 className="text-xl font-bold mb-3 ">Follow Us</h3>
             <div className="flex space-x-4 mb-4">
               <a href="https://github.com/nakuru172000" target="_blank" className=" hover:text-white transition">
                 <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
@@ -46,7 +46,7 @@ const Footer = () => {
                 <svg className="w-5 h-5 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z" />
                 </svg>
-                +1 (555) 123-4567
+                800-9000600
               </li>
               <li className="flex items-center">
                 <svg className="w-5 h-5 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
