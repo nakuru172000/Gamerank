7c6b70b9cab4e975be447c43362216aa614fcd7a
nakuru172000 <150978501+nakuru172000@users.noreply.github.com>
2025-07-04 00:49:13 +0200

    footer down with  flex grow main

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -5,23 +5,38 @@ import BackToTop from './BackToTop';
 import { FaArrowUp } from 'react-icons/fa';
 
 export default function Layout({ children,genres }) {
-  return (
-    <div className="min-h-screen max-w-screen">
-      <div className='sticky top-0 z-50'><Navbar /> 
+  // return (
+  //   <div className="min-h-screen max-w-screen">
+  //     <div className='sticky top-0 z-50'><Navbar /> 
+  //     </div>    
+  //     <div className="container mx-auto px-4 py-8 flex flex-col md:flex-row gap-8">
+  //       {/* Sidebar Container */}
+  //       <div className="md:w-1/4">
+  //         <Sidebar genres={genres}/>
+  //       </div>
+
+  //       {/* Main Content Area */}
+  //       <main className="min-h-screen w-full md:w-3/4">
+  //         {children} {/* This will be your game list, detail page, etc. */}
+  //       </main>
+  //     </div>
+  //     <BackToTop />
+  //     <Footer/>
+  //   </div>
+  // );
+   return (
+
+    <div className="min-h-screen flex flex-col">
+      <div className='sticky top-0 z-50'>
+        <Navbar />
       </div>
-      
-      
-      <div className="container mx-auto px-4 py-8 flex flex-col md:flex-row gap-8">
-        {/* Sidebar Container */}
+      <div className="flex-grow container mx-auto px-4 py-8 flex flex-col md:flex-row gap-8">
         <div className="md:w-1/4">
           <Sidebar genres={genres}/>
         </div>
-
-        {/* Main Content Area */}
         <main className="w-full md:w-3/4">
-          {children} {/* This will be your game list, detail page, etc. */}
+          {children} 
         </main>
-
       </div>
       <BackToTop />
       <Footer/>
